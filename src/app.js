ef9d7523e4c7c7e62996441a37ac3b63de743eb8
Tomas Breffitt <tomasb@tribalworldwide.ca>
2018-04-19 01:14:09 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,69 @@
+import './sass/style.scss'
+import $ from 'jquery';
+const ScrollMagic = require('ScrollMagic');
+require('animation.gsap');
+require('debug.addIndicators');
+const TimelineMax = require('TimelineMax');
+
+$(function() {
+  var controller = new ScrollMagic.Controller();
+
+  var images = [
+    "img/sequence/frame_00.png",
+    "img/sequence/frame_01.png",
+    "img/sequence/frame_02.png",
+    "img/sequence/frame_03.png",
+    "img/sequence/frame_04.png",
+    "img/sequence/frame_05.png",
+    "img/sequence/frame_06.png",
+    "img/sequence/frame_07.png",
+    "img/sequence/frame_08.png",
+    "img/sequence/frame_09.png",
+    "img/sequence/frame_10.png",
+    "img/sequence/frame_11.png",
+    "img/sequence/frame_12.png",
+    "img/sequence/frame_13.png",
+    "img/sequence/frame_14.png",
+    "img/sequence/frame_15.png",
+    "img/sequence/frame_16.png",
+    "img/sequence/frame_17.png",
+    "img/sequence/frame_18.png",
+    "img/sequence/frame_19.png",
+    "img/sequence/frame_20.png"
+  ];
+
+  // TweenMax can tween any property of any object. We use this object to cycle through the array
+  var obj = {curImg: 0};
+
+  // create tween
+  var tween = TweenMax.to(obj, 2,{
+    curImg: images.length - 1,	// animate propery curImg to number of images
+    roundProps: "curImg",				// only integers so it can be used as an array index
+    repeat: 0,									// repeat 3 times
+    immediateRender: true,			// load first image automatically
+    ease: Linear.easeNone,			// show every image the same ammount of time
+    onUpdate: function () {
+      $(".envelope-wrapper > img").attr("src", images[obj.curImg]); // set the image source
+    }
+  });
+
+  new ScrollMagic.Scene({
+    triggerElement: '.envelope',
+      duration: "1000",
+      triggerHook: "onLeave"
+    })
+    .setTween(tween)
+    .setPin(".envelope-wrapper")
+    .addTo(controller);
+
+  for (var i = 1; i < 8; i++) {
+    // create a scene
+    new ScrollMagic.Scene({
+      triggerElement: '#page-wrapper-' + i,
+    		duration: 500,
+        triggerHook: "onLeave"
+    	})
+    	.setPin("#page-" + i)
+    	.addTo(controller);
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  resolve: {
+    alias: {
+        "TweenLite": path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+        "TweenMax": path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+        "TimelineLite": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+        "TimelineMax": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+        "ScrollMagic": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+        "animation.gsap": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+        "debug.addIndicators": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', { modules: false }]
+            ]
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+          jQuery: "jquery",
+          $: "jquery",
+          jquery: "jquery"
+      })
+  ]
+}
+
+module.exports = config
